e9918f3cb703387b3b04d254da96ca0c03a6fe2e
DRWShub <ericgodworks@yahoo.com>
2022-12-08 15:55:57 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ *     */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
