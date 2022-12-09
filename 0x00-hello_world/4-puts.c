6a8904640ab4e603680dad91e03b987f33206e05
DRWShub <ericgodworks@yahoo.com>
2022-12-08 16:18:57 -0800

    hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *Description: prints "Programming is like building a multilingual puzzle"
- *    * Return: Always 0 (Success)
- *     */
+/*
+ *   main - Entry point
+ *   Description: prints "Programming is like building a multilingual puzzle"
+ *   Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle""\n");
