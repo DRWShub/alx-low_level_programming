5e1a351938cac95d13ea4de935750ff9e45d272f
DRWShub <ericgodworks@yahoo.com>
2022-12-09 06:32:34 -0800

    Size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - xxxxxxx
+ *   * main - print the size of various types
+ *     * Description: prints the size of various types on the computer it is compiled
+ *      * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	return (0);
+}
