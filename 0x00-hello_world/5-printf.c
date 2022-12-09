cba25bc8ae5659aa17015327859c4bd5eeeab6ab
DRWShub <ericgodworks@yahoo.com>
2022-12-08 18:58:14 -0800

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,7 @@
 
 /**
    	* main - Entry point
-     	* Description: prints "with proper grammar, but the outcome is a piece of * art,"
-        * Return: Always 0 (Success) 
+     	* Description: prints "with proper grammar, but the outcome is a piece		* Return: Always 0 (Success) 
 */
 
 int main(void)
