917a74e503b37518d2bfe3f115885727dbb217dc
DRWShub <ericgodworks@yahoo.com>
2022-12-09 18:59:55 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
  * main - print the letters of the alphabet
  * Description: print the letters of the alphabet
  * Return: Always 0 (Success)
- * */
+ */
 
 int main(void)
 {
 	int i = 97;
+
 	while (i < 123)
 	{
 		putchar(i);
