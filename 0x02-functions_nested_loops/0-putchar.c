1e1f57cc217d153d6cb00c1b2ff15c4016cfa292
DRWShub <ericgodworks@yahoo.com>
2022-12-13 07:26:17 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *	main - prints _putchar
+ *	Description - program that prints _putchar, followed by a new line.
+ *	Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
