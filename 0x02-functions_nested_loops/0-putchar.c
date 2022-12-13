f2695d49f99177679ac1eee8286f0b7ebb2f85b4
DRWShub <ericgodworks@yahoo.com>
2022-12-13 13:57:58 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *	main - prints Holberton
- *	Description: prints Holberton
+ *	main - prints _putchar, followed by a new line
+ *	Description: prints _putchar
  *	Return: 0 (Success)
  */
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[8] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
