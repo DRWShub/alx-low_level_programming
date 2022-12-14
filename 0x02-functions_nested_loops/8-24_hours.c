cab6e78b66f7826fa34da9b234493d0972de385b
DRWShub <ericgodworks@yahoo.com>
2022-12-14 02:48:38 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	
 	hours_max = 58;
 	hours_tens = '0';
-
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
