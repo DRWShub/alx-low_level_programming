f526c4de49bafc51701f06bb8437ecfa75667207
DRWShub <ericgodworks@yahoo.com>
2022-12-15 10:32:51 -0800

    Like, comment, subscribe

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - test a function that prints positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
-
 	return (0);
 }
