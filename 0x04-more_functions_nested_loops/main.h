fa62ce5ebae9ca8f773dd2161ebb48379ee7a70f
DRWShub <ericgodworks@yahoo.com>
2022-12-15 18:31:19 -0800

    _is_upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-fndef MAIN_H
+#ifdef MAIN_H
 #define MAIN_H
 
-
-int _putchar(char c);
 int _isupper(int c);
+int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
