7101c17face670f2eb23e312f8b1f42938e5ad5d
DRWShub <ericgodworks@yahoo.com>
2022-12-23 14:38:41 -0800

    Push all

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* print_number - p main description */
+
+/**
+ * main - add one line to this code
+ * Return: Always return 0
+ */
 int main(void)
 {
 	int n;
@@ -8,6 +12,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -16,7 +21,8 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	;
+
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
