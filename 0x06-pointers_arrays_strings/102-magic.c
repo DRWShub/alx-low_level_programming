c141fc0a4644272a5d0cacd98f72a91e5d530975
DRWShub <ericgodworks@yahoo.com>
2022-12-23 17:09:52 -0800

    Removed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 1024;
+	a[2] = 98;
 	p = &n;
 	/*
 	 * write your line of code here...
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	n = 98 == *(p + 2);
+	n = *(p + 2) == 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
