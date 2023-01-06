cfcbcfacfb6cdac28da24b7738789cc042dc9370
DRWShub <ericgodworks@yahoo.com>
2023-01-06 15:59:12 -0800

    strcat obj file

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @st:char
+ * @src:char
+ * Return:char
+ */
+
+char *_strcat(char *st, char *src)
+{
+	char *c = st;
+
+	while (*st != '\0')
+	{
+		st++;
+	}
+	while (*src != '\0')
+	{
+		*st = *src;
+		st++;
+		src++;
+	}
+	*st = '\0';
+	return (c);
+}
