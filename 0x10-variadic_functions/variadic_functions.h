294f653e6139e6706266b1cfe83d892e98a49966
DRWShub <ericgodworks@yahoo.com>
2023-01-19 20:01:08 -0800

    functions and the prototype of functions are included in the header file called variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef _VARIADIC_FUNTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * #f: print function
+ */
+
 int sum_them_all(const unsigned int n, ...);
 
 #endif
