95a424a20d862acc04ce3a0768ad80f651a95b54
Eric Godworks <ericgodworks@yahoo.com>
2023-02-06 20:47:31 -0800

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,48 +1,50 @@
 #include "main.h"
-/**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int len = 0;
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
+int _string_len(char *s);
 /**
  * create_file - creates a file
- * @filename: name of file
- * @text_content: text
- * Return: 1 - success, -1 error
+ *
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ *
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	unsigned int length;
-	ssize_t bytes_written;
+	int file;
+	int length = 0;
+	int write_len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		length = 0;
-		text_content = "";
+		length = _string_len(text_content);
+		write_len = write(file, text_content, length);
 	}
-	else
-		length = _strlen(text_content);
-	bytes_written = write(fd, text_content, length);
-	if (bytes_written == -1)
+	if (write_len == -1)
 		return (-1);
-	close(fd);
+	close(file);
 	return (1);
+}
+/**
+ * _string_len - get the length of a string
+ *
+ * @s: string
+ *
+ * Return: length of string
+ *
+ */
+int _string_len(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len))
+		++len;
+	return (len);
 }
