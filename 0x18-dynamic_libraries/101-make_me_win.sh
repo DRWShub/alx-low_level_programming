8a9dbd359f6be39f7d2aa2c9868593b689ebbfa7
Eric Godworks <ericgodworks@yahoo.com>
2023-03-29 08:44:34 -0700

    make me win file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DRWShub/alx-low_level_programming/raw/main/0x18-dynamic_libraries/cracked.so
+wget -P /tmp https://github.com/DRWShub/alx-low_level_programming/blob/master/0x18-dynamic_libraries/cracked.so
 export LD_PRELOAD=/tmp/cracked.so
