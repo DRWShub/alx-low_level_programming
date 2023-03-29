afc9cf12348493df90aeba7799b20094615b3aa2
DRWShub <39622481+DRWShub@users.noreply.github.com>
2023-03-29 17:12:40 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/DRWShub/alx-low_level_programming/blob/master/0x18-dynamic_libraries/cracked.so
-export LD_PRELOAD="$PWD/../cracked.so"
+wget -P .. https://raw.githubusercontent.com/DRWShub/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libcracked.so
+export LD_PRELOAD="$PWD/../libcracked.so"
