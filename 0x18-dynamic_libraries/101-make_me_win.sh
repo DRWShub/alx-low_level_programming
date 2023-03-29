950350f4a5977211bd1aae5b2cb1d2d430fdecaf
Eric Godworks <ericgodworks@yahoo.com>
2023-03-29 09:18:21 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-=======
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/DRWShub/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libcracked.so
-export LD_PRELOAD="$PWD/../libcracked.so"
->>>>>>> afc9cf12348493df90aeba7799b20094615b3aa2
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
